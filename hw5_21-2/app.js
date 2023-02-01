df72393c42c4d12400437c26a9380a85f4b6a995
Nuraiym20 <114159583+Nuraiym20@users.noreply.github.com>
2023-02-01 22:05:16 +0600

    Update app.js

--- hw5_21-2/app.js
+++ hw5_21-2/app.js
@@ -2,15 +2,37 @@ const getData = document.querySelector('.Get')
 const wrapper  = document.querySelector(".wrapper")
 
 getData.addEventListener('click' , () => {
-    const request = new XMLHttpRequest
-    console.log(request)
-    request.open('GET','data.json')
-    request.setRequestHeader('Content-type','application/json')
-    request.send()
+    // const request = new XMLHttpRequest
+    // console.log(request)
+    // request.open('GET','data.json')
+    // request.setRequestHeader('Content-type','application/json')
+    // request.send()
 
-    request.addEventListener('load', () => {
-        const data = JSON.parse(request.response)
+    // request.addEventListener('load', () => {
+    //     const data = JSON.parse(request.response)
 
+//         data.map((item) => {
+//             const block = document.createElement('div')
+//             const div = document.createElement('span')
+//             const img = document.createElement('img')
+//             block.innerHTML = item.title
+//             div.innerHTML = item.price
+//             img.src = item.img
+
+//             wrapper.append(div)
+//             wrapper.append(block)
+//             wrapper.append(img)
+        
+
+//         })
+//     })
+// })
+
+fetch("data.json")
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
         data.map((item) => {
             const block = document.createElement('div')
             const div = document.createElement('span')
@@ -22,8 +44,6 @@ getData.addEventListener('click' , () => {
             wrapper.append(div)
             wrapper.append(block)
             wrapper.append(img)
-        
-
         })
     })
 })
